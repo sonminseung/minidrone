e6455687000442baec9a9e9a201daf03a9475c18
minseung <shon628@naver.com>
2025-07-15 11:39:06 +0900

    회귀분석 업데이트

--- stage1_fitting.m
+++ stage1_fitting.m
@@ -6,19 +6,20 @@ center_a = [480, 260];
 drone = ryze();
 cam = camera(drone);
 takeoff(drone);
-%멀때  error_pixel
+
+
 Err_pixel = 30;
+circleflag = 0;
 
+Err_pixel_near = 60;
 
-
-%가까울때 error_pixel
-Err_pixel_1 = 40;
 moveup(drone,'Distance',0.7,'Speed',1);
 pause(3);
 
 
 %% 1. 맨처음 calibration
 while 1
+
     % 1-1) 이미지 획득 & HSV 분리
     [frame , ~] = snapshot(cam);
     hsv = rgb2hsv(frame);
@@ -32,7 +33,7 @@ while 1
 
     % 1-3) 원 검출 시도
     binary_res = xor(blue_mask_clean, 1);
-    stats = regionprops(binary_res, 'Centroid', 'Circularity', 'Area');
+    stats = regionprops(binary_res, 'Centroid', 'Circularity', 'Area','MajorAxisLength','MinorAxisLength');
     props_blue =  regionprops(blue_mask_clean, 'BoundingBox');
     bbox = props_blue(1).BoundingBox;
     width = bbox(3);
@@ -55,61 +56,95 @@ while 1
         areas     = [stats(idxCircle).Area];
 
         % 면적이 1000 이상인 것만 선택
-        largeMask  = areas >= 1000;          % logical mask
+        largeMask  = areas >= 700;          % logical mask
         idxLarge   = idxCircle(largeMask);   % stats 인덱스
         areasLarge = areas(largeMask);       % 필터된 면적값
 
         if isempty(idxLarge)
-            warning("면적 ≥1000인 원형 후보가 없습니다.");
-            % (여기서 대체 로직을 넣어도 좋습니다)
-        else
-            % 필터된 후보 중 가장 큰 녀석 찾기
-            [~, relMax] = max(areasLarge);
-            idx         = idxLarge(relMax);
+            warning("면적 ≥700인 원형 후보가 없습니다.");
 
-            centers = stats(idx).Centroid;
-            disp("원 중심 사용")
+            % (여기서 대체 로직을 넣어도 좋습니다)
         end
+
+        % 필터된 후보 중 가장 큰 녀석 찾기
+        [~, relMax] = max(areasLarge);
+        idx         = idxLarge(relMax);
+        centers = stats(idx).Centroid;
+        disp("원 중심 사용")
     end
 
     imshow(frame); hold on
     plot(centers(1), centers(2), 'ro', 'MarkerSize', 12, 'LineWidth', 2);
     hold off
-
-    % 1-4) 화면 중심과의 오차 계산
+    pause(3);
+    % 1-4) 화면 중심과의 오차 계산(가까운 거리에서)
     dis = centers - center_pts;
+    if width == 960
+        % 1-5) 좌우 회전
+        if abs(dis(1)) < Err_pixel_near && abs(dis(2)) < Err_pixel_near
+            disp("캘리브레이션 완료 — 루프 탈출");
+            Major = stats(idx).MajorAxisLength;
+            Minor = stats(idx).MinorAxisLength;
 
-    if abs(dis(1)) < Err_pixel && abs(dis(2)) < Err_pixel
-        disp("캘리브레이션 완료 — 루프 탈출");
-        break;
-    end
-    % 1-5) 좌우 회전
-    if dis(1) > Err_pixel
-        moveright(drone,'Distance',0.2);
-        fprintf("move right\n");
-    elseif dis(1) < -Err_pixel
-        moveleft(drone,'Distance',0.2);
-        fprintf("move left\n");
-    end
+            Avg_diameter = (Major+Minor) / 2;
+            circleflag = 1;
+            break;
+        end
+        if dis(1) > Err_pixel_near
+            moveright(drone,'Distance',0.2);
+            fprintf("move right\n");
+        elseif dis(1) < -Err_pixel_near
+            moveleft(drone,'Distance',0.2);
+            fprintf("move left\n");
+        end
 
-    % 1-6) 상하 이동
-    if dis(2) > Err_pixel
-        movedown(drone,'Distance',0.2);
-        fprintf("move down\n");
-    elseif dis(2) < -Err_pixel
-        moveup(drone,'Distance',0.2);
-        fprintf("move up\n");
+        % 1-6) 상하 이동
+        if dis(2) > Err_pixel_near
+            movedown(drone,'Distance',0.2);
+            fprintf("move down\n");
+        elseif dis(2) < -Err_pixel_near
+            moveup(drone,'Distance',0.2);
+            fprintf("move up\n");
+        end
+    else
+        if abs(dis(1)) < Err_pixel && abs(dis(2)) < Err_pixel
+            disp("캘리브레이션 완료 — 루프 탈출");
+            break;
+            % 1-5) 좌우 회전
+        elseif dis(1) > Err_pixel
+            moveright(drone,'Distance',0.2);
+            fprintf("move right\n");
+        elseif dis(1) < -Err_pixel
+            moveleft(drone,'Distance',0.2);
+            fprintf("move left\n");
+        end
+
+        % 1-6) 상하 이동
+        if dis(2) > Err_pixel
+            movedown(drone,'Distance',0.2);
+            fprintf("move down\n");
+        elseif dis(2) < -Err_pixel
+            moveup(drone,'Distance',0.2);
+            fprintf("move up\n");
+        end
     end
 end
-
 %% 원통과 일단 시켜보기
 x = input('f == 전진 t == 스탑','s');
 if x == 'f'
-    dist = 6.376*exp(-0.001567*width);
-    dist = round(dist,1);
-    fprintf("%f", dist);
-    dist = dist+0.4;
-    moveforward(drone, 'Distance',dist, 'Speed',1);
+    if circleflag == 1
+        dist = 67.63*exp(-0.02768*Avg_diameter)+3.229*exp(-0.00246*Avg_diameter);
+        dist = round(dist,1);
+        fprintf("%f", dist);
+        dist = dist+0.4;
+        moveforward(drone, 'Distance',dist, 'Speed',1);
+    else
+        dist = 6.376*exp(-0.001567*width);
+        dist = round(dist,1);
+        fprintf("%f", dist);
+        dist = dist+0.4;
+        moveforward(drone, 'Distance',dist, 'Speed',1);
+    end
 
 elseif x == 't'
     land(drone)
